c127ac5f6eacfbc4d46cb811fea2d407270ebe8a
Olof Svahn <svahnolof@gmail.com>
2021-05-03 16:28:28 +0200

    Fixes

---
+++ base.js
@@ -0,0 +1,67 @@
+let currentAdress = window.location.href;
+let currentURL = new URL(window.location.href);
+
+let searchParams = new URLSearchParams(currentURL.searchParams);
+
+let firstname = searchParams.get("firstname");
+let email = searchParams.get("email");
+let password = searchParams.get("password");
+let birthdate = searchParams.get("birthdate");
+let isidiot = searchParams.get("idiot");
+let hasidiotparents = searchParams.get("idiotparents");
+let dumbchoice = searchParams.get("dumbchoice");
+let dumbchoices = [
+    "Kinda stoopid",
+    "Pretty f-ing dumb",
+    "Very stupid",
+    "Dumber than someone who is very dumb",
+    "Håkan Hellström-level moron",
+    "Practically brain dead",
+    "Actually brain dead",
+];
+
+document.getElementById("fullname").innerHTML = "Here's your result, " + firstname;
+
+let infoString = "First name: " + firstname + 
+"<br>" + "Email: " + email +
+"<br>" + "Birthdate: " + birthdate;
+
+let stupidinfo = "";
+
+let stupidinfo2 = dumbchoices[dumbchoice];
+
+if(isidiot == "on")
+{
+    stupidinfo += "You are a certified moron";
+}
+else
+{
+    stupidinfo += "You are NOT a certified moron";
+}
+
+if(hasidiotparents == "on")
+{
+    if(isidiot == "on")
+    {
+        stupidinfo += " and you also come from two idiot parents";
+    }
+    else
+    {
+        stupidinfo += ", but you come from two idiot parents";
+    }
+}
+else
+{
+    if(isidiot == "on")
+    {
+        stupidinfo += ", but your parents have normal IQ, which is too bad for you";
+    }
+    else
+    {
+        stupidinfo += " and your parents are not unsmart... get it?";
+    }
+}
+
+document.getElementById("info").innerHTML = infoString;
+document.getElementById("stupidinfo").innerHTML = stupidinfo;
+document.getElementById("stupidinfo2").innerHTML = stupidinfo2;
---
+++ validate.js
@@ -0,0 +1,54 @@
+function Validate()
+{
+    let firstname = document.forms["form"]["firstname"].value;
+    let email = document.forms["form"]["email"].value;
+    let password = document.forms["form"]["password"].value;
+    let choice = document.forms["form"]["dumbchoice"].value;
+
+    let birthdate = new Date(document.forms["form"]["birthdate"].value);
+    let birthYear = birthdate.getFullYear();
+    let today = new Date();
+    let currentYear = today.getFullYear();
+    let age = currentYear - birthYear;
+
+    let lettersRegex = /^[a-zA-Z-,]+(\s{0,1}[a-zA-Z-, ])*$/;
+    let emailRegex = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
+    let passwordRegex = /^^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
+
+    if(!firstname.match(lettersRegex))
+    {
+        alert("Name cannot contain numbers!");
+        return false;
+    }
+
+    if(!email.match(emailRegex))
+    {
+        alert("Email is incorrect!");
+        return false;
+    }
+
+    if(!password.match(passwordRegex))
+    {
+        alert("Password must have minimum eight characters, at least one letter and one number")
+        return false;
+    }
+
+    if(choice == "0")
+    {
+        alert("Please choose a dumbness level!");
+        return false;
+    }
+
+    if(age > 130)
+    {
+        alert("You are too damn old!");
+        return false;
+    }
+    else if(age < 5)
+    {
+        alert("You are too young, lol afk omg!");
+        return false;
+    }
+
+    return true;
+}
