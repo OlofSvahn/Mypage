49fd4ba4d2b8d3b326bb43b05e68bee9114152ec
Olof Svahn <svahnolof@gmail.com>
2021-05-04 14:27:55 +0200

    Update slideshow.js

--- slideshow.js
+++ slideshow.js
@@ -3,6 +3,7 @@ var i = 0; var path = new Array();
 path[0] = "https://github.com/OlofSvahn/Mypage/blob/main/SP/africa-139343__340.jpg"; 
 path[1] = "https://github.com/OlofSvahn/Mypage/blob/main/SP/animal-197161__340.jpg"; 
 path[2] = "https://github.com/OlofSvahn/Mypage/blob/main/SP/animals-1975258__340.jpg"; 
+path[3] = ""
 
 function imageChange() 
 { 
